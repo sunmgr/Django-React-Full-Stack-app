68d20f58b3f44e497057685e69ba5e15f5558c6e
Sunash Magar <magarsunash005@gmail.com>
2025-11-26 22:31:14 +0545

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,12 @@
 import axios from  'axios'
 import { ACCESS_TOKEN  } from './constants'
 
+const apiUrl = "/choreo-apis/djangoreact/backend/v1"
+
 const api = axios.create({
 
-    baseURL: import.meta.env.VITE_API_URL
-})
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+});
 
 api.interceptors.request.use(
     (config) => {
